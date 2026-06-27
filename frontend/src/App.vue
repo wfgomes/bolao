3b1b9765814d96333f95f3b05aaa849c844baaf3
Wesckley <wesckley.gomes@gmail.com>
2026-06-27 13:14:40 -0300

    menu adm

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="app-wrapper">
+  <div class="app-wrapper" @click="closeMenu">
 
     <header v-if="auth.token" class="top-header">
       <span class="header-brand">⚽ Bolão dos Amigos</span>
       <div class="header-right">
         <router-link to="/perfil" class="header-user">{{ auth.user?.name }}</router-link>
+        <!-- Hamburguer: só mobile + admin -->
+        <button v-if="auth.isAdmin" class="btn-hamburger" @click.stop="menuOpen = !menuOpen" :class="{ active: menuOpen }">
+          <span></span><span></span><span></span>
+        </button>
         <button @click="logout" class="btn-logout">Sair</button>
       </div>
     </header>
+
+    <!-- Menu dropdown mobile para admin -->
+    <nav v-if="auth.token && auth.isAdmin && menuOpen" class="admin-mobile-menu" @click.stop>
+      <router-link to="/admin/jogos"        @click="menuOpen = false">⚽ Jogos</router-link>
+      <router-link to="/admin/resultados"   @click="menuOpen = false">📋 Resultados</router-link>
+      <router-link to="/admin/fases"        @click="menuOpen = false">🔒 Fases</router-link>
+      <router-link to="/admin/usuarios"     @click="menuOpen = false">👥 Usuários</router-link>
+      <router-link to="/admin/classificacao" @click="menuOpen = false">🏆 Classificação</router-link>
+      <router-link to="/admin/artilheiros"  @click="menuOpen = false">🥇 Artilheiros</router-link>
+      <router-link to="/partidas"           @click="menuOpen = false">📊 Acertos</router-link>
+    </nav>
 
     <!-- Nav desktop para admin e usuário -->
     <nav v-if="auth.token" class="desktop-nav">
@@ -53,11 +68,15 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useAuthStore } from './stores/auth'
 import { useRouter } from 'vue-router'
 
-const auth   = useAuthStore()
-const router = useRouter()
+const auth     = useAuthStore()
+const router   = useRouter()
+const menuOpen = ref(false)
+
+function closeMenu() { menuOpen.value = false }
 
 function logout() {
   auth.logout()
